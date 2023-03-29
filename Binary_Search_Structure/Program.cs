570fda128d27bb28121f974f49e88c88bfa3c14b
JackRipper01 <francohernandezpiloto@gmail.com>
2023-03-28 22:42:00 -0400

    abb done

--- Binary_Search_Structure/Program.cs
+++ Binary_Search_Structure/Program.cs
@@ -17,34 +17,12 @@ new Binary_Tree<int>(1,
 
     );
 int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+int[] arrayRegado = { 3, 1, 7, 5, 8, 3, 9, 10, 11, 4, 17, 15, 19, 20 };
+int[] arrrray = { 5, 6, 1, 2, 4, 3 };
 
-Binary_Tree<int> test = Make_Tree(array);
+Binary_Tree<int> test = Binary_Tree<int>.Create_Binary_Tree_From_Sorted_Array_Efficiently(arrrray);
+
+
 
 Console.WriteLine("END OF PROGRAM");
-static Binary_Tree<int> Make_Tree(int[] array)
-{
-    Binary_Tree<int> result = new Binary_Tree<int>(array[array.Length / 2]);
-    Make_Treee(array, result);
-    return result;
-}
-static void Make_Treee(int[] array, Binary_Tree<int> result)
-{
-    int[] left_array = array[0..(array.Length / 2)];
 
-    int[] right_array = array[(array.Length / 2 == 1 ? array.Length / 2 : array.Length / 2 + 1)..array.Length];
-
-    if (left_array.Length == 1 && left_array[0] != result.Key)
-    {
-        result.Left = new Binary_Tree<int>(left_array[0]);
-        return;
-    }
-    else
-        Make_Treee(left_array, result.Left = new Binary_Tree<int>(left_array[left_array.Length / 2]));
-
-    if (right_array.Length == 1 && right_array[0] != result.Key)
-        result.Right = new Binary_Tree<int>(right_array[0]);
-    else
-        Make_Treee(right_array, result.Right = new Binary_Tree<int>(right_array[right_array.Length / 2]));
-
-
-}
--- Binary_Search_Structure/Structure.cs
+++ Binary_Search_Structure/Structure.cs
@@ -1,22 +1,137 @@
 namespace Binary_Tree_Structure;
 
-public class Binary_Tree<T>
+public class Binary_Tree<T> where T : IComparable
 {
-    public int Key { get; set; }
+    public T Key { get; set; }
 
+    public Binary_Tree<T>? Parent;
     public Binary_Tree<T>? Left;
     public Binary_Tree<T>? Right;
 
-    public Binary_Tree(int key)
+    public Binary_Tree(T key)
     {
         Key = key;
 
     }
-    public Binary_Tree(int key, Binary_Tree<T> left, Binary_Tree<T> right)
+    public Binary_Tree(T key, Binary_Tree<T> left, Binary_Tree<T> right)
     {
         Key = key;
         Left = left;
         Right = right;
     }
+    public void Insert(T entry)
+    {
+        if (entry.CompareTo(Key) == 0)
+            return;
+        if (entry.CompareTo(Key) == -1)
+        {
+            if (Left == null)
+                Left = new Binary_Tree<T>(entry);
+            else
+                Left.Insert(entry);
+        }
+        else if (entry.CompareTo(Key) == 1)
+        {
+            if (Right == null)
+                Right = new Binary_Tree<T>(entry);
+            else
+                Right.Insert(entry);
+        }
+    }
+    public static Binary_Tree<int> Create_Binary_Tree_From_Sorted_Array_Efficiently(int[] array)
+    {
+        Binary_Tree<int> result = new Binary_Tree<int>(array[array.Length / 2]);
+        Make_Treee(array, result);
+        return result;
+    }
+    static void Make_Treee(int[] array, Binary_Tree<int> result)
+    {
+        int[] left_array = array[0..(array.Length / 2)];
+
+        int[] right_array = array[(array.Length / 2 == 1 ? array.Length / 2 : array.Length / 2 + 1)..array.Length];
+
+        if (left_array.Length == 1 && left_array[0] != result.Key)
+        {
+            result.Left = new Binary_Tree<int>(left_array[0]);
+            return;
+        }
+        else
+            Make_Treee(left_array, result.Left = new Binary_Tree<int>(left_array[left_array.Length / 2]));
+
+        if (right_array.Length == 1 && right_array[0] != result.Key)
+            result.Right = new Binary_Tree<int>(right_array[0]);
+        else
+            Make_Treee(right_array, result.Right = new Binary_Tree<int>(right_array[right_array.Length / 2]));
+    }
+
+    //falta avl
+
+    public static Binary_Tree<int> Create_Binary_Tree(int[] array)
+    {
+        Binary_Tree<int> result = new Binary_Tree<int>(array[0]);
+        for (int i = 1; i < array.Length; i++)
+        {
+            result.Insert(array[i]);
+        }
+        return result;
+    }
+
+    public void Eliminate(T entry)
+    {
+        if (Left != null && Left.Key.CompareTo(entry) == 0)
+        {
+            if (IsLeaf(Left))
+                Left = null;
+            else if (HasOneChildren(Left))
+                Left = GetOnlyChildren(Left);
+            else
+            {
+                Left = GetLargestMinor(Left);
+            }
+        }
+        else if (Right != null && Right.Key.CompareTo(entry) == 0)
+        {
+            if (IsLeaf(Right))
+                Right = null;
+            else if (HasOneChildren(Right))
+                Right = GetOnlyChildren(Right);
+            else
+            {
+                Right = GetLargestMinor(Right);
+            }
+        }
+        else
+        {
+            if (Left != null && entry.CompareTo(Key) == -1)
+                Left.Eliminate(entry);
+            else if (Right != null && entry.CompareTo(Key) == 1)
+                Right.Eliminate(entry);
+        }
+        return;
+    }
+    bool IsLeaf(Binary_Tree<T> node) => node.Left == null && node.Right == null;
+    bool HasOneChildren(Binary_Tree<T> node) => (node.Left != null && node.Right == null) || (node.Left == null && node.Right != null);
+    Binary_Tree<T> GetOnlyChildren(Binary_Tree<T> node)
+    {
+        if (node.Left != null)
+            return node.Left;
+        else if (node.Right != null)
+            return node.Right;
+        throw new Exception(message: "empty node");
+    }
+
+    Binary_Tree<T> GetLargestMinor(Binary_Tree<T> node)
+    {
+        if (node.Left == null && node.Right != null)
+            return node.Right;
+        else if (node.Left != null)
+        {
+            return GetLargestMinor(node.Left);
+        }
+        throw new Exception();
+    }
+
+
+
 
 }
